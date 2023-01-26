d4c77f2ff19486974c18a5d321d197857934acb3
esbar <esbarr@softserveinc.com>
2023-01-26 06:58:37 -0500

    terraform changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
 provider "aws" {
   region = "us-west-2"
 }
@@ -6,16 +16,6 @@ resource "aws_instance" "example" {
   ami           = var.image_id
   instance_type = "t2.micro"
 
-  tags = {
-    Name = "example"
-  }
-}
-
-resource "aws_ami" "example" {
-  name                = "example"
-  description         = "example ami"
-  virtualization_type = "hvm"
-  source_instance_id  = aws_instance.example.id
   tags = {
     Name = "example"
   }
